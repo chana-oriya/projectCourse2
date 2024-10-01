cff89eae13f1f988967e07b56ac95ee763a1be2c
tamar-nisan <tamarnisan@hilms.tech>
2024-10-01 10:58:52 +0300

    finalfinal

--- logIn/logIn.js
+++ logIn/logIn.js
@@ -7,10 +7,9 @@ function onSubmit(event){
     let password=document.getElementById("password").value;
    
     let users = JSON.parse(localStorage.getItem("users"))
-    console.log(users);
+   
     let currentUser=users.find(user=>{
-        console.log('user: ', user);
-        console.log( (user.username===name) && (user.password===password));
+    
         return user.username===name && user.password===password
     })
 
